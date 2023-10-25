35e1a3d6a094a6df337565eeb8d678440cb8fbe3
Danish <danishsalim321@gmail.com>
2023-10-25 12:30:02 +0530

    task5

--- sharpener/task3/dom.js
+++ sharpener/task3/dom.js
@@ -1,38 +1,11 @@
-//examine the document object
-// console.log(document.domain)
-// console.log(document.title)
-// document.title=666
-// console.log(document.head)
-// console.log(document.body)
-// console.log(document.all)
-// console.log(document.all[10])
-// document.all[10].textContent="hello"
-//  console.log(document.forms)
-//  console.log(document.forms[0])
 
-//  console.log(document.forms[0][1].value="hello")
-//  console.log(document.links)
+let item5=document.getElementsByClassName('new-list')
+console.log(item5);
+item5[0].style.color='green'
+let newItems=document.getElementsByTagName('li')
+console.log(newItems[4])
+newItems[4].textContent='newItem5'
 
- let headerTitle= document.getElementById('header-title')
-// console.log(headerTitle.innerText)
-// headerTitle.innerText="innerText"
-// console.log(headerTitle.textContent)
-// headerTitle.textContent="textContent"
-// console.log(headerTitle.innerHTML)
-//  headerTitle.innerHTML="innerHTML"
-// headerTitle.innerHTML= '<h6>innerHtmlwithH6</h6>'
-// let header=document.querySelector('header')
-// header.style.borderBottom='solid 5px #000'
-// let formTitle=document.querySelector(".title")
-// formTitle.style.fontWeight='bold'
-// formTitle.style.color='green'
 
-let items= document.getElementsByClassName('list-group-item')
-console.log(items)
-items[2].style.backgroundColor='green'
- 
-for(let i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight='bold'
-}
+
 
