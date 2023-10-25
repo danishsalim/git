bcaf23c6c5541673fd12367278a74b649ac50ea7
Danish <danishsalim321@gmail.com>
2023-10-25 13:02:55 +0530

    task6

--- sharpener/task3/dom.js
+++ sharpener/task3/dom.js
@@ -1,10 +1,18 @@
 
-let item5=document.getElementsByClassName('new-list')
-console.log(item5);
-item5[0].style.color='green'
-let newItems=document.getElementsByTagName('li')
-console.log(newItems[4])
-newItems[4].textContent='newItem5'
+let item2=document.querySelector('.list-group-item:nth-child(2)')
+console.log(item2)
+item2.style.backgroundColor='green'
+let item3=document.querySelector('.list-group-item:nth-child(3)')
+item3.style.display='none'
+let items=document.querySelectorAll('.list-group-item')
+items[1].style.color='green'
+let odd = document.querySelectorAll('li:nth-child(odd)')
+console.log(odd)
+for(let item of odd)
+{
+   item.style.backgroundColor='green'
+}
+
 
 
 
