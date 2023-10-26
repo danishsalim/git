dae3f31c69cae7ce7eeec4c918fcaa8ec7f3f5aa
Danish <danishsalim321@gmail.com>
2023-10-26 13:58:25 +0530

    added task 11

---
+++ sharpener/task11/main.js
@@ -0,0 +1,28 @@
+let form=document.querySelector('form')
+let msg=document.querySelector('.msg')
+let name=document.querySelector('#name')
+let email=document.querySelector('#email')
+let subBtn=document.querySelector('#submit')
+let users=document.querySelector('.userlists')
+
+subBtn.addEventListener('mouseover',()=>subBtn.style.background='orange')
+
+form.addEventListener('submit',validate)
+
+
+function validate(e)
+{
+    e.preventDefault()
+   if(name.value!="" && email.value!="")
+   {
+    let mydetail=JSON.stringify({name:name.value,email:email.value})
+    localStorage.setItem("userDetails",mydetail)
+    console.log(JSON.parse(localStorage.getItem('userDetails')))
+   }
+   else{
+        console.log('please fill all input fields')
+        msg.innerHTML='please fill all input fields'
+        msg.style.color='red'
+        setTimeout(()=>msg.innerHTML="",3000)
+   }
+}
